644481c3a08dc60b0f01fd961c955d5f123e335f
Arno Pigeon <git@sysody.eu>
2021-09-14 19:04:11 +0200

    avancement flux

---
+++ src/store.ts
@@ -0,0 +1,45 @@
+import {writable} from 'svelte/store'
+
+type CashFlow = {
+    date: string, // YYYY-MM-DD
+    amount: number,
+    account: string,
+    event: string,
+    type: string,
+    details: string,
+    ref: string,
+    note: string
+}
+
+export const cashFlows =writable([
+    {
+        date: "2021-06-18",
+        amount: -5.25,
+        account: "Compte courant",
+        event: "Sans event",
+        type: "Fonctionnement hors évènement",
+        details: "Frais de gestion de compte",
+        ref: "BNP0008",
+        note: ""
+    },
+    {
+        date: "2021-06-19",
+        amount: -3.89,
+        account: "Compte courant",
+        event: "Sans event",
+        type: "Fonctionnement hors évènement",
+        details: "Frais de carte de débit",
+        ref: "BNP0008",
+        note: ""
+    },
+    {
+        date: "2021-06-24",
+        amount: -41.58,
+        account: "Compte courant",
+        event: "Sans event",
+        type: "Fonctionnement hors évènement",
+        details: "Matériel, technique et autres frais",
+        ref: "BNP0021",
+        note: "Gel hydroalcoolique"
+    },
+])
--- src/tabs/flux.svelte
+++ src/tabs/flux.svelte
@@ -1,1 +1,28 @@
-flux
+<script lang="ts">
+import {cashFlows} from "../store"
+
+const cashFlowTitles = [
+    ["date", "Date"],
+    ["amount", "Montant"],
+    ["account", "Compte"],
+    ["event", "Évenement"],
+    ["nature", "Nature"],
+    ["details", "Détails"],
+    ["ref", "Réference"],
+    ["note", "Remarque"]
+]
+</script>
+<table>
+    <tr>
+        {#each cashFlowTitles as item}
+            <th>{item[1]}</th>
+        {/each}
+    </tr>
+    {#each $cashFlows as flow}
+        <tr>
+        {#each cashFlowTitles as item}
+            <td>{flow[item[0]]}</td>
+        {/each}
+        </tr>
+    {/each}
+</table>
