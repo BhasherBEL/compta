d87109216b0d2458893ea5fabd2764e056fce4fc
Arno Pigeon <git@sysody.eu>
2021-09-15 10:13:22 +0200

    creation/modification/suppression flux

---
+++ src/components/icon.svelte
@@ -0,0 +1,14 @@
+<script>
+    export let library = "octicons"
+    export let icon;
+    export let size=42;
+    export let color="#000000"
+</script>
+
+<img style="height:{size}px; width:{size}px" alt="icon {icon}" src="https://icongr.am/{library}/{icon}.svg?size={size}&color={color}">
+
+<style>
+    img {
+        max-width: none;
+    }
+</style>
--- src/store.ts
+++ src/store.ts
@@ -11,35 +11,44 @@ type CashFlow = {
     note: string
 }
 
-export const cashFlows =writable([
-    {
-        date: "2021-06-18",
-        amount: -5.25,
-        account: "Compte courant",
-        event: "Sans event",
-        type: "Fonctionnement hors évènement",
-        details: "Frais de gestion de compte",
-        ref: "BNP0008",
-        note: ""
-    },
-    {
-        date: "2021-06-19",
-        amount: -3.89,
-        account: "Compte courant",
-        event: "Sans event",
-        type: "Fonctionnement hors évènement",
-        details: "Frais de carte de débit",
-        ref: "BNP0008",
-        note: ""
-    },
-    {
-        date: "2021-06-24",
-        amount: -41.58,
-        account: "Compte courant",
-        event: "Sans event",
-        type: "Fonctionnement hors évènement",
-        details: "Matériel, technique et autres frais",
-        ref: "BNP0021",
-        note: "Gel hydroalcoolique"
-    },
-])
+function createCashFlows() {
+    const {subscribe, update} = writable([
+        {
+            date: "2021-06-18",
+            amount: -5.25,
+            account: "Compte courant",
+            event: "Sans event",
+            type: "Fonctionnement hors évènement",
+            details: "Frais de gestion de compte",
+            ref: "BNP0008",
+            note: ""
+        },
+        {
+            date: "2021-06-19",
+            amount: -3.89,
+            account: "Compte courant",
+            event: "Sans event",
+            type: "Fonctionnement hors évènement",
+            details: "Frais de carte de débit",
+            ref: "BNP0008",
+            note: ""
+        },
+        {
+            date: "2021-06-24",
+            amount: -41.58,
+            account: "Compte courant",
+            event: "Sans event",
+            type: "Fonctionnement hors évènement",
+            details: "Matériel, technique et autres frais",
+            ref: "BNP0021",
+            note: "Gel hydroalcoolique"
+        }
+    ])
+    return {
+        subscribe,
+        push: (elem) => update((c) => [...c, elem]),
+        remove: (i) => update(c => c.filter((el, index) => i !== index))
+    }
+}
+
+export const cashFlows = createCashFlows()
