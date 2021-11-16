3141474e9a37d363835ded9ec3d030aefe5245b4
Arno Pigeon <git@sysody.eu>
2021-11-16 17:01:02 +0100

    adding new features to editableTable.svelte

--- src/components/editableTable.svelte
+++ src/components/editableTable.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
     import type { Writable } from "svelte/store"
     import type { GenericColumn } from "../utils"
-    import EditableValue from "./editableValue.svelte";
-    import Icon from "./icon.svelte";
+    import EditableValue from "./editableValue.svelte"
+    import Icon from "./icon.svelte"
     export let colgroup = []
     export let columns: {[index: string]: GenericColumn}
     export let dataStore: Writable<Object>;
-    export let validateChange: (arg0: Object) => boolean;
+    export let validateChange: (arg0: Object) => boolean
+    export let addNew: (arg0: Object) => void
     let dataBeingEdited = []
+    let newData = {}
 
     function toggleEditable(index: number) {
         if (dataBeingEdited.includes(index)) {
@@ -48,7 +50,7 @@
                     {/if}
                 </td>
             {/each}
-            <th class="grouped gapless">
+            <th class="grouped gapless pull-right">
                 <button class="button outline icon-only" on:click={() => toggleEditable(index)} disabled="{!validateChange(data)}">
                     <Icon icon="pencil"/>
                 </button>
@@ -59,4 +61,22 @@
             </th>
         </tr>
     {/each}
+    <tr>
+        <form id="new-data" on:submit|preventDefault={addNew}></form>
+        {#each Object.entries(columns) as [key, item]}
+            <td>
+            {#if item.nature === "input"}
+                <EditableValue bind:value={newData[key]} type="{item.type}" placeholder="{item.name}" required="{item.required}" form="new-data-"/>
+                {:else }
+                <i class="text-grey">Valeur calculée</i>
+                {/if}
+            </td>
+            {/each}
+        <td>
+            <label class="button icon-only">
+                <input type="submit" class="is-hidden" form="account-new">
+                <Icon icon="plus"/>
+            </label>
+        </td>
+    </tr>
 </table>
